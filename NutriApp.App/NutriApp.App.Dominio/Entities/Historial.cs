c5940787f26f3740d24ebd48be51e55127b19ecf
Carlos Soto <carlos950216@gmail.com>
2021-09-22 18:48:20 -0500

    Create Entities

---
+++ NutriApp.App/NutriApp.App.Dominio/Entities/Historial.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace NutriApp.App.Dominio{
+    
+    public class Historial{
+        public int Id {get;set;}
+        public float Carbohidratos {get;set;}
+        public float Proteinas {get;set;}
+        public float Grasas {get;set;}
+        public float Peso {get;set;}
+        public float Estatura {get;set;}
+        public Seguimiento Seguimiento {get;set;}
+    }
+}
---
+++ NutriApp.App/NutriApp.App.Dominio/Entities/Nutricionista.cs
@@ -0,0 +1,6 @@
+namespace NutriApp.App.Dominio{
+    public class Nutricionista:Persona{
+        public string Especializacion {get;set;}
+        public string TarjetaProfesional {get;set;}
+    }
+}
