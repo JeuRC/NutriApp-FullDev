96689b0ff7d3b2742e81380c1f3fa7ef54cb963b
Carlos Soto <carlos950216@gmail.com>
2021-09-22 20:00:25 -0500

    Create migrations and database tables

---
+++ NutriApp.App/NutriApp.App.Persistencia/Migrations/20210923005341_FirstMigration.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NutriApp.App.Persistencia.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Seguimientos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seguimientos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Historiales",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Carbohidratos = table.Column<float>(type: "real", nullable: false),
+                    Proteinas = table.Column<float>(type: "real", nullable: false),
+                    Grasas = table.Column<float>(type: "real", nullable: false),
+                    Peso = table.Column<float>(type: "real", nullable: false),
+                    Estatura = table.Column<float>(type: "real", nullable: false),
+                    SeguimientoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Historiales", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Historiales_Seguimientos_SeguimientoId",
+                        column: x => x.SeguimientoId,
+                        principalTable: "Seguimientos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Personas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Coach_Especializacion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Coach_TarjetaProfesional = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Especializacion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TarjetaProfesional = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NutricionistaId = table.Column<int>(type: "int", nullable: true),
+                    CoachId = table.Column<int>(type: "int", nullable: true),
+                    HistorialId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Personas_Historiales_HistorialId",
+                        column: x => x.HistorialId,
+                        principalTable: "Historiales",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Personas_Personas_CoachId",
+                        column: x => x.CoachId,
+                        principalTable: "Personas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Personas_Personas_NutricionistaId",
+                        column: x => x.NutricionistaId,
+                        principalTable: "Personas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Historiales_SeguimientoId",
+                table: "Historiales",
+                column: "SeguimientoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personas_CoachId",
+                table: "Personas",
+                column: "CoachId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personas_HistorialId",
+                table: "Personas",
+                column: "HistorialId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personas_NutricionistaId",
+                table: "Personas",
+                column: "NutricionistaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Personas");
+
+            migrationBuilder.DropTable(
+                name: "Historiales");
+
+            migrationBuilder.DropTable(
+                name: "Seguimientos");
+        }
+    }
+}
